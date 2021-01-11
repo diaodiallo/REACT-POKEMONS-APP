2b154f0b83c8798dd8acdb310ab005d638918df2
Diao Diallo <diao_diallo@gn.jsi.com>
2021-01-11 14:11:57 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from 'react';
+import PokemonList from './pages/pokemon-list';
+import PokemonDetail from './pages/pokemon-details';
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PageNotFound from './pages/page-not-found';
+import PokemonEdit from './pages/pokemon-edit';
+
+
+const App: FunctionComponent = () => {
+  
+  return (
+    <Router>
+      <div>
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">Pokédex</Link>
+          </div>
+        </nav>
+      <Switch>
+        <Route exact path="/" component={PokemonList}/>
+        <Route exact path="/pokemons" component={PokemonList}/>
+        <Route exact path="/pokemons/edit/:id" component={PokemonEdit}/>
+        <Route path="/pokemons/:id" component={PokemonDetail}/>
+        <Route component={PageNotFound}/>
+      </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
